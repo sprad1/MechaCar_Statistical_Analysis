02deb9529b85180c6f5c4a31f4df8c3ceecdb7e6
sprad1 <86751774+sprad1@users.noreply.github.com>
2021-10-10 21:01:23 -0500

    updated with Deliverable 2

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -2,7 +2,7 @@
 # 3. Use library() function to load dplyr package:
 library(dplyr)
 
-# 4. Import and read MecharCar_mpg.csv
+# 4. Import and read MechaCar_mpg.csv
 mecha_car_table = read.csv(file="MechaCar_mpg.csv",check.names=F,stringsAsFactors = F)
 
 # 5. Perform linear regression using lm() function and pass in all six variables(columns) and add dataframe from step 4 as data parameter
@@ -12,3 +12,20 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_car_table)
 )
 
+### Deliverable 2
+# 2. Import and read in the Suspension_Coil.csv file as a table
+suspension_table = read.csv(file="Suspension_Coil.csv",check.names = F,stringsAsFactors = F)
+
+# 3. Write RScript that creates total_summary dataframe using summarize() to get mean, median, variance, and standard deviation of PSI column
+total_summary <- suspension_table %>% summarize(Mean_PSI=mean(PSI),
+                                                Median_PSI=median(PSI),
+                                                Var_PSI=var(PSI),
+                                                Std_Dev_PSI=sd(PSI),
+                                                Num_Suspension=n(), .groups = 'keep')
+
+# 4. Write RScript that creates lot_summary dataframe using group_by() & summarize() to group each manufacturing lot by mean, median, variance, & standard deviation of PSI column
+lot_summary <- suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI),
+                                                                              Median_PSI=median(PSI),
+                                                                              Var_PSI=var(PSI),
+                                                                              Std_Dev_PSI=sd(PSI),
+                                                                              Num_Suspension=n(), .groups = 'keep')
